7ea33a3dce7c8ca26769b50341671008da4393c5
Diego Vargas <68403877+dvardcr@users.noreply.github.com>
2022-03-26 11:02:07 -0600

    Revert "Ricardo"

--- index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", ()=> {
-    const sidebar = document.querySelector(".sidebar");
-    var div_shape = document.createElement("div");
-    
-    div_shape.setAttribute("id", "extra");
-    div_shape.setAttribute("style", "width: 0; height: 0; border-left: 50px solid transparent; border-right: 50px solid transparent; border-bottom: 100px solid red; position: relative;")
-    
-    sidebar.appendChild(div_shape);
-})
